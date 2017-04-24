4bb865d5a8f4a8207c8e8d42eb5dc71736e4a4c7
JohnScarrow <johna@scarrows.com>
2017-04-24 14:44:56 -0700

    working on gamerTag Dom

--- results.js
+++ results.js
@@ -5,14 +5,31 @@ function resultDisplay(){
   var avatarUserWrap = document.createElement('div');
   var userOnlineWrap = document.createElement('div');
   var gamerGamesWrap = document.createElement('div');
+  var avatarPic = document.createElement('div');
   var username = document.createElement('h3');
   var userOnline = document.createElement('h3');
   var gamertags = document.createElement('ul');
   var gamesPlayed = document.createElement('ul');
-  var discription = document.createElement('p');
+  var comments = document.createElement('p');
   var contactButton = document.createElement('button');
 
   player.className = 'player';
   results = document.appendChild(player);
-  
+  player = document.appendChild(avatarUserWrap);
+  avatarUserWrap = document.appendChild(avatarPic);
+  avatarUserWrap = document.appendChild(userOnlineWrap);
+  userOnlineWrap = document.appendChild(username);
+  userOnlineWrap = document.appendChild(userOnline);
+  player = document.appendChild(gamerGamesWrap);
+  gamerGamesWrap = document.appendChild(gamertags);
+  gamerGamesWrap = document.appendChild(gamesPlayed);
+  player = document.appendChild(comments);
+  player = document.appendChild(contactButton);
+
+
+  // Gamertag Creation for div
+  for (var i = 0; i < object.entries(playerList.gamerTags).length; i++){
+    var gamerTagLi = createElement('li');
+    var gamerTagLiText = new textNode (object.entries(playerList[j].gamerTags[i][0], ': ', object.entries(playerList[j].gamerTags[i][1])))
 }
+    
