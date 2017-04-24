3a79e587b3d581829962d61f0e3559a4cbecfb27
JohnScarrow <johna@scarrows.com>
2017-04-24 12:52:57 -0700

    first save

---
+++ results.js
@@ -0,0 +1,18 @@
+'use strict';
+function resultDisplay(){
+  var results = document.getElementById('results');
+  var player = document.createElement('div');
+  var avatarUserWrap = document.createElement('div');
+  var userOnlineWrap = document.createElement('div');
+  var gamerGamesWrap = document.createElement('div');
+  var username = document.createElement('h3');
+  var userOnline = document.createElement('h3');
+  var gamertags = document.createElement('ul');
+  var gamesPlayed = document.createElement('ul');
+  var discription = document.createElement('p');
+  var contactButton = document.createElement('button');
+
+  player.className = 'player';
+  results = document.appendChild(player);
+  
+}
